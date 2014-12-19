034561e60b9d14bed9d7e0e7b570602ff263bc18
Sebastian Luy <sebastian.a.luy@gmail.com>
2014-12-19 12:32:56 -0500

    Initial commit

---
+++ app/src/main/java/com/wonderguerrilla/android/workout/MainActivity.java
@@ -0,0 +1,66 @@
+package com.wonderguerrilla.android.workout;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    private TextView mExerciseView ;
+    private Button mNextButton ;
+    private Button mPreviousButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mExerciseView = (TextView)findViewById(R.id.workout_text_view) ;
+        mExerciseView.setText(Workout.getCurrentExercise()) ;
+
+        mNextButton = (Button)findViewById(R.id.next_button) ;
+        mNextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mExerciseView.setText(Workout.getNextExercise()) ;
+            }
+        });
+
+        mPreviousButton = (Button)findViewById(R.id.previous_button) ;
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mExerciseView.setText(Workout.getPreviousExercise()) ;
+            }
+
+        });
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/wonderguerrilla/android/workout/Workout.java
@@ -0,0 +1,49 @@
+package com.wonderguerrilla.android.workout;
+
+/**
+ * Created by sebluy on 12/19/14.
+ */
+public class Workout {
+
+    private static int[] exercises = {
+            R.string.bench_press,
+            R.string.seated_row_pull,
+            R.string.shoulder_press,
+            R.string.lat_pull_down,
+            R.string.tricep_extension,
+            R.string.bicep_curl,
+            R.string.deltoid_lateral_raise,
+            R.string.upright_row,
+            R.string.thumbs_down,
+            R.string.external_rotation,
+            R.string.internal_rotation,
+            R.string.Y,
+            R.string.T,
+            R.string.W
+    } ;
+
+    private static int index = 0 ;
+
+    private static void incrementIndex() {
+        index = (index + 1) % exercises.length ;
+    }
+
+    private static void decrementIndex() {
+        index = index == 0 ? exercises.length - 1 : index - 1 ;
+    }
+
+    public static int getCurrentExercise() {
+        return exercises[index] ;
+    }
+
+    public static int getNextExercise() {
+        incrementIndex() ;
+        return getCurrentExercise() ;
+    }
+
+    public static int getPreviousExercise() {
+        decrementIndex() ;
+        return getCurrentExercise() ;
+    }
+
+}
