853f1ac41ca3458c91412e8caab57e6a66d90c3e
Sebastian Luy <sebastian.a.luy@gmail.com>
2015-01-12 20:35:55 -0500

    Add stubs for running, swimming and basketball workouts

--- app/src/main/java/com/wonderguerrilla/android/workout/WorkoutGenerator.java
+++ app/src/main/java/com/wonderguerrilla/android/workout/WorkoutGenerator.java
@@ -4,13 +4,17 @@ import android.content.Context;
 
 public class WorkoutGenerator {
 
+    private static final String RUN = "Run Workout" ;
+    private static final String SWIM = "Swim Workout" ;
+    private static final String BASKETBALL = "Basketball" ;
+
     private static String[] sWorkoutNames = {
         LiftingWorkout.UPPER_NAME,
-        "Run Workout",
+        RUN,
         CalisthenicWorkout.NAME,
-        "Swim Workout",
+        SWIM,
         LiftingWorkout.LOWER_NAME,
-        "Basketball",
+        BASKETBALL,
         StaticCoreWorkout.STATIC_CORE_NAME
     } ;
 
@@ -20,16 +24,16 @@ public class WorkoutGenerator {
 
     public static Workout create(String workoutName, Context context) {
         switch (workoutName) {
-            case CalisthenicWorkout.NAME:
-                return CalisthenicWorkout.generate(context) ;
             case LiftingWorkout.UPPER_NAME:
                 return LiftingWorkout.newUpperLiftingWorkout(context) ;
+            case CalisthenicWorkout.NAME:
+                return CalisthenicWorkout.generate(context) ;
             case LiftingWorkout.LOWER_NAME:
                 return LiftingWorkout.newLowerLiftingWorkout(context) ;
             case StaticCoreWorkout.STATIC_CORE_NAME :
                 return StaticCoreWorkout.newStaticCoreWorkout() ;
             default:
-                return null ;
+                return new Workout(workoutName, null) ;
         }
     }
 
